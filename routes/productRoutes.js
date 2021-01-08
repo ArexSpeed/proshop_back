08a95e2dc8f8be27ffe8b34f07388a57b0569f31
Arek Cichocki <arexspeed95@gmail.com>
2021-01-08 13:55:41 +0100

    findById

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -16,7 +16,7 @@ router.get('/', asyncHandler(async(req,res) => {
 // @route   GET /api/products/:id
 // @access  Public
 router.get('/:id', asyncHandler(async(req,res) => {
-  const product = await Product.find(req.params.id)
+  const product = await Product.findById(req.params.id)
   if(product){
     res.json(product)
   }
